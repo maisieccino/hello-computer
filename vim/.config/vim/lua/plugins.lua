94417a64d8d2845bd3d11b265a1b6b45c08ef3d2
Maisie Bell <4870168+maisieccino@users.noreply.github.com>
2025-03-13 10:44:54 +0000

    Update

--- vim/.config/vim/lua/plugins.lua
+++ vim/.config/vim/lua/plugins.lua
@@ -1,18 +1,14 @@
 return {
 	{
 		'dracula/vim',
-		name = "dracula",
+		name = 'dracula',
 		lazy = true,
 		config = function()
-			vim.cmd.colorscheme("dracula")
-		end
+			vim.cmd.colorscheme('dracula')
+		end,
 	},
-	{
-		'rafi.plugins.extras.coding.copilot',
-		enabled = false,
-	},
-	{
-		dir = "~/src/github.com/monzo/wearedev/tools/editors/nvim/nvim-monzo",
-		name = "monzo",
-	}
+	-- {
+	-- 	dir = "~/src/github.com/monzo/wearedev/tools/editors/nvim/nvim-monzo",
+	-- 	name = "monzo",
+	-- }
 }
