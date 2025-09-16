b28bfce3ba8a39770823f2816513ee1c79632750
Maisie Bell <4870168+maisieccino@users.noreply.github.com>
2025-09-16 16:39:34 +0100

    Fix snacks and octo

--- vim/.config/nvim/lua/plugins/octo.lua
+++ vim/.config/nvim/lua/plugins/octo.lua
@@ -2,11 +2,15 @@ return {
   { import = "lazyvim.plugins.extras.util.octo" },
   {
     "octo.nvim",
-    -- Disable error popups
     opts = {
+      -- Fixes issues with diffs being wrong(?)
+      use_local_fs = true,
+      picker = "snacks",
+      -- Disable error popups
       suppress_missing_scope = {
         projects_v2 = true,
       },
+      users = "mentionable",
     },
   },
 }
