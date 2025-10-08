df9ebfcc20b3d47d021af27a94afee19e4aca1d4
Maisie Bell <4870168+maisieccino@users.noreply.github.com>
2025-10-08 14:45:31 +0100

    Only start astro lsp when needed

--- vim/.config/nvim/after/lsp/astro.lua
+++ vim/.config/nvim/after/lsp/astro.lua
@@ -1,4 +1,10 @@
 -- Problem running? Make sure that typescript is added as a dev dependency to
 -- your Yarn project.
 ---@type vim.lsp.Config
-return {}
+return {
+  root_markers = {
+    "package.json",
+    ".git",
+  },
+  filetypes = { "astro" },
+}
