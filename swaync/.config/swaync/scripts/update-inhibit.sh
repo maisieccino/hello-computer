6f513924f6ef6605b618dddf9ee89921399d9ef8
Maisie Bell <4870168+maisieccino@users.noreply.github.com>
2026-01-24 16:32:39 +0000

    Move swaync to its own stow

---
+++ swaync/.config/swaync/scripts/update-inhibit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if pgrep -f "systemd-inhibit --what=idle sleep infinity" >/dev/null; then
+	echo true
+else
+	echo false
+fi
---
+++ swaync/.config/swaync/scripts/update-mic.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if pactl get-source-mute @DEFAULT_SOURCE@ | grep -q "Mute: yes"; then
+	echo true
+else
+	echo false
+fi
