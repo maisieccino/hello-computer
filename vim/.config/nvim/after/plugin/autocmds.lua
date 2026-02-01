f19f8febf33f2351cee4a0ee39ca94c12b5392e5
Maisie Bell <4870168+maisieccino@users.noreply.github.com>
2026-02-01 15:37:28 +0000

    Frontmatter parsing

--- vim/.config/nvim/after/plugin/autocmds.lua
+++ vim/.config/nvim/after/plugin/autocmds.lua
@@ -1,3 +1,6 @@
+local util = require("util")
+local Snacks = require("snacks")
+
 -- automatically import output chunks from a jupyter notebook
 -- tries to find a kernel that matches the kernel in the jupyter notebook
 -- falls back to a kernel that matches the name of the active venv (if any)
@@ -49,4 +52,77 @@ vim.api.nvim_create_autocmd("BufWritePost", {
       vim.cmd("MoltenExportOutput!")
     end
   end,
+})
+
+local function to_obsidian_attachment_path(path, src)
+  local dirname = util.git_root_dir(path)
+  local filename = vim.fn.fnamemodify(src, ":t")
+
+  local matches = vim.fs.find(filename, {
+    path = dirname .. "/Assets",
+    limit = 1,
+  })
+  if #matches == 0 then
+    return src
+  else
+    return matches[1]
+  end
+end
+
+-- gets images for frontmatter
+local function get_imgs()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local ts = vim.treesitter
+  local query = ts.query.parse(
+    "markdown",
+    [[
+((minus_metadata) @frontmatter)
+]]
+  )
+  for _ in query:iter_captures(ts.get_parser():trees()[1]:root(), bufnr) do
+    -- Parse yaml
+    local fm_query = ts.query.parse(
+      "yaml",
+      [[
+          (block_mapping_pair
+            value: (flow_node
+            (plain_scalar (string_scalar) @a)
+            (#match? @a "\.(jpe?g|png|tiff)$")
+            )
+          )
+        ]]
+    )
+    local tree = ts.get_parser():children()["yaml"]:trees()[1]
+    if tree == nil then
+      vim.print("no tree found")
+      return
+    end
+
+    Snacks.image.placement.clean(bufnr)
+    for _, match in fm_query:iter_captures(tree:root(), bufnr) do
+      local file_name = ts.get_node_text(match, bufnr)
+
+      local status, obsidian = pcall(require, "obsidian.api")
+      local path = vim.api.nvim_buf_get_name(bufnr)
+      if status and obsidian.path_is_note(path) then
+        -- Resolve obsidian img file
+        file_name = to_obsidian_attachment_path(path, file_name)
+      end
+
+      local row = match:range(false)
+      Snacks.image.placement.new(bufnr, file_name, {
+        pos = { row + 1, 0 },
+        range = { row + 1, 0, row + 1, 0 },
+        inline = true,
+        max_height = 20,
+        auto_resize = true,
+        conceal = false,
+      })
+    end
+  end
+end
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
+  pattern = { "*.md" },
+  callback = get_imgs,
 })
